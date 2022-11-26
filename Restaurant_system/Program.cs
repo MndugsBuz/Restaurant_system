6ddbf7fc8ad634c009846736c1fb5bc2be61a304
MndugsBuz <mindaugas.bauza@codeacademylt.onmicrosoft.com>
2022-11-27 01:44:40 +0200

    UserInterface

--- Restaurant_system/Program.cs
+++ Restaurant_system/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Restaurant_system;
+
 Console.WriteLine("-- Restaurant System --");
 
-
+UserInterface userInterface = new UserInterface();
+userInterface.Menu();
+Console.WriteLine("Your Choise");
+Console.ReadLine();
---
+++ Restaurant_system/UserInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_system
+{
+    internal class UserInterface
+    {
+
+        public void Menu()
+        {
+            Console.WriteLine("Menu [1]: Waiter [1] Waiters Table List [2]");
+            Console.WriteLine("Menu [2]: Visitor [1] Visitor Order [2]");
+            Console.WriteLine("Menu [3]: Order [1] Report orders List [2]");
+            Console.WriteLine("Menu [4]: Table [1] Rport table List [2]");
+            Console.WriteLine("Menu [5]: Product [1] Product List [2]" );
+
+        }
+    }
+
+    
+}
