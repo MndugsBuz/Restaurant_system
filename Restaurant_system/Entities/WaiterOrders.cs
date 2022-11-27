08badc2d3fbd2d99407829d573ad731633c1732d
MndugsBuz <mindaugas.bauza@codeacademylt.onmicrosoft.com>
2022-11-27 05:07:57 +0200

    Add WaiterOrders Repository

--- Restaurant_system/Entities/WaiterOrders.cs
+++ Restaurant_system/Entities/WaiterOrders.cs
@@ -6,26 +6,26 @@ using System.Threading.Tasks;
 
 namespace Restaurant_system.Entities
 {
-    public class Waiter<T1, T2, T3, T4, T5>
+    public class WaiterOrders<T1, T2, T3, T4, T5>
     {
         public T1 WaiterId { get; set; }
         public T2 Name { get; set; }
         public T3 Table { get; set; }
         public T4 VisitorId { get; set; }
-        public T5 VisitorOrder { get; set; }
+        public T5 VisitorOrderNumber { get; set; }
 
-        public Waiter ()
+        public WaiterOrders ()
         {
 
         }
 
-        public Waiter (T1 waiterId, T2 name, T3 table, T4 visitorId, T5 visitorOrder)
+        public WaiterOrders (T1 waiterId, T2 name, T3 table, T4 visitorId, T5 visitorOrderNumber)
         {
             this.WaiterId = waiterId;
             this.Name = name;
             this.Table = table;
             this.VisitorId = visitorId;
-            this.VisitorOrder = visitorOrder;
+            this.VisitorOrderNumber = visitorOrderNumber;
         }
     }
 }
---
+++ Restaurant_system/Repositories/WaiterOrdersRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Restaurant_system.Entities;
+
+namespace Restaurant_system.Repositories
+{
+    public class WaiterOrdersRepository;
+    {
+       private List<WaiterOrders> waiterOrders { get; set; } = new List<WaiterOrders>();
+
+    }
+}
