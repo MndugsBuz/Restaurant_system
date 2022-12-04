55fbbd72f27f64a08dc693dcba1d53a510830a3d
MndugsBuz <mindaugas.bauza@codeacademylt.onmicrosoft.com>
2022-12-04 20:32:26 +0200

    Add Entities Food, Drinks

---
+++ Restaurant_system/Entities/Drinks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_system.Entities
+{
+    public class Drinks
+    {
+        {
+        public string DrinkID { get; set; }
+        public string DrinkName { get; set; }
+        public string DrinkDescription { get; set; }
+        public string UnitofMesuarment { get; set; }
+        public decimal DrinkPrice { get; set; }
+
+        public Drinks()
+        {
+        }
+
+        public Drinks(string drinkId, string drinkName, string drinkDescription, string unitOfMesuarment, decimal drinkprice)
+
+        {
+            this.DrinkID = drinkId;
+            this.DrinkName = drinkName;
+            this.DrinkDescription = drinkDescription;
+            this.UnitofMesuarment = unitOfMesuarment;
+            this.DrinkPrice = drinkprice;
+
+        }
+
+    }
+}
+}
---
+++ Restaurant_system/Entities/Food.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_system.Entities
+{
+    public class Food
+    {
+        public string FoodID { get; set; }
+        public string FoodName { get; set; }
+        public string FoodDescription { get; set; }
+        public string UnitofMesuarment { get; set; }
+        public decimal FoodPrice { get; set; }
+
+        public Food ()
+            { 
+            }
+
+        public Food (string foodId, string foodName, string foodDescription, string unitOfMesuarment, decimal foodprice)
+
+        {
+            this.FoodID = foodId;
+            this.FoodName = foodName;   
+            this.FoodDescription = foodDescription;
+            this.UnitofMesuarment = unitOfMesuarment;
+            this.FoodPrice = foodprice;
+
+        }
+
+    }
+}
